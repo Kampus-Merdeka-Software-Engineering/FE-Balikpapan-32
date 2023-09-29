9203fe0770395515b78b33947afbd97c64707cb5
itsmedeva <devafajar14@gmail.com>
2023-09-29 21:55:18 +0700

    menambahkan javascript pada beranda

--- js/beranda.js
+++ js/beranda.js
@@ -1,4 +1,23 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function (){
+dropDownMenu.classList.toggle('open')
+const isOpen = dropDownMenu.classList.contains('open')
+
+toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
 const loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+const socialMedia = document.getElementById("social");
+const myFunction = (e) => {
+    if(!confirm('Apakah anda yakin ingin meninggalkan halaman ini?')) e.preventDefault();
+}
+
+socialMedia.addEventListener('click', myFunction, false);
